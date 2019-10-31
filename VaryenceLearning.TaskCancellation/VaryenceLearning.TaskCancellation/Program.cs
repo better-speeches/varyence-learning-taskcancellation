5ccbeb3484e0cfc1dc535b5cbab134cffe979003
Alex K <alexk@varyence.com>
2019-10-31 14:43:25 +0200

    Initial commit

---
+++ VaryenceLearning.TaskCancellation/VaryenceLearning.TaskCancellation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VaryenceLearning.TaskCancellation
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
